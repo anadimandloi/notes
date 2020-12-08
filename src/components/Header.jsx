917546285783c20af637bb9ce470228490d6d813
Anadi Mandloi <anadim1540@gmail.com>
2020-12-08 23:54:03 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassIcon from '@material-ui/icons/Class';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <ClassIcon />
+        Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
